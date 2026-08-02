45ead82298530fa70fe918263a174c3b2ee2990f
dharanika0723 <dharanika2301@gmail.com>
2026-08-02 20:46:34 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 
 /* =====================================================
